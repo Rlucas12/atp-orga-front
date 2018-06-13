a5afd0c06ba1173cc9a2e86084843d51508dfb47
REYNES Lucas <lucasreynes@gmail.com>
2018-06-13 09:40:18 +0200

    Update gitignore

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_URL: '"https://gentle-badlands-33351.herokuapp.com/api"'
 })
